6157e891d15504cc1b260d07f1347793be444331
Bobby <Bobby1985uk@gmail.com>
2019-09-08 19:55:16 +0100

    fix: naming convention on file casing

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,11 +1,11 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
-import Content, { HTMLContent } from "../components/Content"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
+import Content, { HTMLContent } from '../components/content';
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+  const PageContent = contentComponent || Content;
 
   return (
     <Layout>
@@ -16,17 +16,17 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
         </div>
       </section>
     </Layout>
-  )
-}
+  );
+};
 
 AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
-}
+};
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <AboutPageTemplate
@@ -34,14 +34,14 @@ const AboutPage = ({ data }) => {
       title={post.frontmatter.title}
       content={post.html}
     />
-  )
-}
+  );
+};
 
 AboutPage.propTypes = {
   data: PropTypes.object.isRequired,
-}
+};
 
-export default AboutPage
+export default AboutPage;
 
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
@@ -52,4 +52,4 @@ export const aboutPageQuery = graphql`
       }
     }
   }
-`
+`;
