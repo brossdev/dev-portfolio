c252efcd9d7acfed01c0cd33940930ca68327f32
Bobby <Bobby1985uk@gmail.com>
2019-09-11 20:07:20 +0100

    feat: add starter thanks page

---
+++ src/pages/contact/thanks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../../components/layout';
+import SEO from '../../components/seo';
+
+const Thanks = () => (
+  <Layout>
+    <SEO title="Thanks" />
+    <h1>Thanks</h1>
+    <p>Your email has been received</p>
+  </Layout>
+);
+
+export default Thanks;
